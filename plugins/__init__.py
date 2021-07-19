488ca6a4646156fe3ead0473d19417dd825569e2
kanekiken44 <81549240+kanekiken44@users.noreply.github.com>
2021-07-19 16:03:56 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -43,14 +43,13 @@ NOSPAM_CHAT = [
 ]
 
 KANGING_STR = [
-    "Using Witchery to kang this sticker...",
-    "Plagiarising hehe...",
-    "Inviting this sticker over to my pack...",
-    "Kanging this sticker...",
-    "Hey that's a nice sticker!\nMind if I kang?!..",
-    "Hehe me stel ur stiker...",
-    "Ay look over there (☉｡☉)!→\nWhile I kang this...",
-    "Roses are red violets are blue, kanging this sticker so my pack looks cool",
-    "Imprisoning this sticker...",
-    "Mr.Steal-Your-Sticker is stealing this sticker... ",
+    "Blood thirsty...",
+    "Kaneki takes your sticker...",
+    "Do you know kagune?...",
+    "Hey that's a nice sticker!\nimma will take it!..",
+    "Kaneki will steal ur Stiker hehe...",
+    "Ay look over there (☉｡☉)!→\nimma Will take it...",
+    "Kaneki's pack will look cool now",
+    "imma is Imprisoning this sticker...",
+    "Mr.Kaneki is stealing this sticker... ",
 ]
